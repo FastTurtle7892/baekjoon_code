e0de3d235a89c0a2dd16ba8d9dbc49151749ecae
Fast_Turtle <158424095+FastTurtle7892@users.noreply.github.com>
2025-11-08 22:33:43 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -10,18 +10,17 @@ using namespace std;
 
 long long dp[1'000'001];
 
-long long process(int input) {
+void process() {
 
     dp[1] = 1;
     dp[2] = 2;
     dp[3] = 4;
 
-    for (int i = 4; i <= input; i++) {
+    for (int i = 4; i < 1'000'001; i++) {
 
         dp[i] = (dp[i - 3] + dp[i - 2] + dp[i - 1]) % 1'000'000'009;
         dp[i] %= 1'000'000'009;
     }
-    return dp[input];
 }
 
 int main() {
@@ -29,12 +28,14 @@ int main() {
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
     int N; cin >> N;
-    
+ 
+    process();
+
     int input;
     for (int i = 0; i < N; i++) {
 
         cin >> input;
-        cout << process(input) << "\n";
+        cout << dp[input] << "\n";
     }
 
     return 0;
